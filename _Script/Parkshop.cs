d37360e8f3296c5061eb4821c22cb875df18fed6
catdoglove <naktop88@gmail.com>
2020-09-04 16:00:56 +0900

    공원 코딩중

--- _Script/Parkshop.cs
+++ _Script/Parkshop.cs
@@ -16,10 +16,20 @@ public class Parkshop : MonoBehaviour
     int shop_rnd;
     int item_num;
     int fstBuy, secBuy;
+
+    public GameObject popUp_obj;
+
+    int cost_r, cost_h, level, sum, have_r, have_h;
+
+    string str_Code;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        str_Code = PlayerPrefs.GetString("code", "");
+
+        shop();
     }
     
     void shop()
@@ -167,5 +177,234 @@ public class Parkshop : MonoBehaviour
                 }
                 break;
         }
+    }
+
+
+
+    public void upFirst()
+    {
+
+        have_r = PlayerPrefs.GetInt(str_Code + "r", 0);
+        have_h = PlayerPrefs.GetInt(str_Code + "h", 0);
+
+        if (fstBuy == 0)
+        {
+            switch (shop_rnd)
+            {
+                case 0: //시계액자
+                    item_num = PlayerPrefs.GetInt("clock", 0);
+                    if (item_num == 5)
+                    {
+                    }
+                    else if (have_h >= cost_clock[item_num * 2] && have_r >= cost_clock[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_clock[item_num * 2];
+                        have_r = have_r - cost_clock[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt(str_Code + "h", have_h);
+                        PlayerPrefs.SetInt(str_Code + "r", have_r);
+                        PlayerPrefs.SetInt("clock", item_num);
+                        txt_fst[0].text = "Lv.X";
+                        txt_fst[1].text = "품절";
+                        txt_fst[2].text = "X";
+                        txt_fst[3].text = "X";
+                        fstBuy = 1;
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+                case 1://액자그림
+                    item_num = PlayerPrefs.GetInt("frame", 0);
+                    if (item_num == 5)
+                    {
+                    }
+                    else if (have_h >= cost_frame[item_num * 2] && have_r >= cost_frame[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_frame[item_num * 2];
+                        have_r = have_r - cost_frame[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt(str_Code + "h", have_h);
+                        PlayerPrefs.SetInt(str_Code + "r", have_r);
+                        PlayerPrefs.SetInt("frame", item_num);
+                        txt_fst[0].text = "Lv.X";
+                        txt_fst[1].text = "품절";
+                        txt_fst[2].text = "X";
+                        txt_fst[3].text = "X";
+                        fstBuy = 1;
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+                case 2://시계그림
+                    item_num = PlayerPrefs.GetInt("clock", 0);
+                    if (item_num == 5)
+                    {
+                    }
+                    else if (have_h >= cost_clock[item_num * 2] && have_r >= cost_clock[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_clock[item_num * 2];
+                        have_r = have_r - cost_clock[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt("coin", have_h);
+                        PlayerPrefs.SetInt("have_r", have_r);
+                        PlayerPrefs.SetInt("clock", item_num);
+                        txt_fst[0].text = "Lv.X";
+                        txt_fst[1].text = "품절";
+                        txt_fst[2].text = "X";
+                        txt_fst[3].text = "X";
+                        fstBuy = 1;
+                        if (item_num == 4)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+
+                default:
+                    item_num = PlayerPrefs.GetInt("clock", 0);
+                    if (item_num == 5)
+                    {
+                    }
+                    else if (have_h >= cost_clock[item_num * 2] && have_r >= cost_clock[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_clock[item_num * 2];
+                        have_r = have_r - cost_clock[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt("coin", have_h);
+                        PlayerPrefs.SetInt("have_r", have_r);
+                        PlayerPrefs.SetInt("clock", item_num);
+                        txt_fst[0].text = "Lv.X";
+                        txt_fst[1].text = "품절";
+                        txt_fst[2].text = "X";
+                        txt_fst[3].text = "X";
+                        fstBuy = 1;
+                        if (item_num == 4)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+            }
+        }
+
+        PlayerPrefs.Save();
+    }
+    public void upSecnd()
+    {
+        if (secBuy == 0)
+        {
+            switch (shop_rnd)
+            {
+                case 0: //시계액자
+                    item_num = PlayerPrefs.GetInt("frame", 0);
+                    if (item_num == 5)
+                    {
+                    }
+                    else if (have_h >= cost_frame[item_num * 2] && have_r >= cost_frame[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_frame[item_num * 2];
+                        have_r = have_r - cost_frame[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt("coin", have_h);
+                        PlayerPrefs.SetInt("have_r", have_r);
+                        PlayerPrefs.SetInt("frame", item_num);
+                        txt_sec[0].text = "Lv.X";
+                        txt_sec[1].text = "품절";
+                        txt_sec[2].text = "X";
+                        txt_sec[3].text = "X";
+                        secBuy = 1;
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+                case 1://액자그림
+                    item_num = PlayerPrefs.GetInt("draw", 0);
+                    if (item_num == 9)
+                    {
+                    }
+                    else if (have_h >= cost_draw[item_num * 2] && have_r >= cost_draw[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_draw[item_num * 2];
+                        have_r = have_r - cost_draw[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt("coin", have_h);
+                        PlayerPrefs.SetInt("have_r", have_r);
+                        PlayerPrefs.SetInt("draw", item_num);
+                        txt_sec[0].text = "Lv.X";
+                        txt_sec[1].text = "품절";
+                        txt_sec[2].text = "X";
+                        txt_sec[3].text = "X";
+                        secBuy = 1;
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+                case 2://시계그림
+                    item_num = PlayerPrefs.GetInt("draw", 0);
+                    if (item_num == 9)
+                    {
+                    }
+                    else if (have_h >= cost_draw[item_num * 2] && have_r >= cost_draw[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_draw[item_num * 2];
+                        have_r = have_r - cost_draw[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt("coin", have_h);
+                        PlayerPrefs.SetInt("have_r", have_r);
+                        PlayerPrefs.SetInt("draw", item_num);
+                        txt_sec[0].text = "Lv.X";
+                        txt_sec[1].text = "품절";
+                        txt_sec[2].text = "X";
+                        txt_sec[3].text = "X";
+                        secBuy = 1;
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+
+                default:
+                    item_num = PlayerPrefs.GetInt("draw", 0);
+                    if (item_num == 9)
+                    {
+                    }
+                    else if (have_h >= cost_draw[item_num * 2] && have_r >= cost_draw[item_num * 2 + 1])
+                    {
+                        have_h = have_h - cost_draw[item_num * 2];
+                        have_r = have_r - cost_draw[item_num * 2 + 1];
+                        item_num++;
+                        PlayerPrefs.SetInt("coin", have_h);
+                        PlayerPrefs.SetInt("have_r", have_r);
+                        PlayerPrefs.SetInt("draw", item_num);
+                        txt_sec[0].text = "Lv.X";
+                        txt_sec[1].text = "품절";
+                        txt_sec[2].text = "X";
+                        txt_sec[3].text = "X";
+                        secBuy = 1;
+                    }
+                    else
+                    {
+                        popUp_obj.SetActive(true);
+                    }
+                    break;
+            }
+        }
+
+        PlayerPrefs.Save();
     }
 }
