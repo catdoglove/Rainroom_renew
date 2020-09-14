f56603f37168a1ac05171389bc6f54a110fa429e
catdoglove <34932357+catdoglove@users.noreply.github.com>
2020-09-14 16:02:33 +0900

    도시 움직임 구현

--- _Script/CityShop.cs
+++ _Script/CityShop.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CityShop : MonoBehaviour
 {
+    public GameObject shop_obj, todayShop_obj, shopHelp_obj, doorOpen_obj, doorClose_obj;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,35 @@ public class CityShop : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void openShop()
     {
-        
+        shop_obj.SetActive(true);
+        doorOpen_obj.SetActive(true);
+        doorClose_obj.SetActive(false);
+    } 
+    
+
+    public void closeShop()
+    {
+        shop_obj.SetActive(false);
+        todayShop_obj.SetActive(false);
+        doorClose_obj.SetActive(true);
+        doorOpen_obj.SetActive(false);
+    }
+
+    public void openTodayShop()
+    {
+        todayShop_obj.SetActive(true);
+    }
+
+    public void openHelp()
+    {
+        shopHelp_obj.SetActive(true);
+    }
+
+
+    public void closeHelp()
+    {
+        shopHelp_obj.SetActive(false);
     }
 }
