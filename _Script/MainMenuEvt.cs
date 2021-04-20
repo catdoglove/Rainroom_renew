fb9fe69fa0b28c066de79e9fd2e8b7b9b77c3ad7
catdoglove <34932357+catdoglove@users.noreply.github.com>
2021-04-20 14:21:50 +0900

    개인정보 링크 버튼 추가

--- _Script/MainMenuEvt.cs
+++ _Script/MainMenuEvt.cs
@@ -349,5 +349,11 @@ public class MainMenuEvt : MonoBehaviour
             PlayerPrefs.SetFloat("talkspeed", 0.07f);
         }
     }
-    
+
+
+    public void showInfoLink()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1JFdyCym-5Kxns2xcA-w8W5ir5YiL4J-6JrJeMF8zcuk/edit?usp=sharing");
+    }
+
 }
