ef77f6cc30e87abaa214548c261ac828f289ee68
catdoglove <34932357+catdoglove@users.noreply.github.com>
2021-07-26 15:50:43 +0900

    뒤로가기 기능

---
+++ _Script/BackbtnEvt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackbtnEvt : MonoBehaviour
+{
+    public GameObject[] wnd;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            for(int i = 0; i < wnd.Length; i++)
+            wnd[i].SetActive(false);
+        }
+
+    }
+}
