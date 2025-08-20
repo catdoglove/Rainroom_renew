9a4ebf7a8ee446edd57ae62ce1bba08f4125b493
catdoglove <34932357+catdoglove@users.noreply.github.com>
2025-08-20 15:28:18 +0900

    애드몹 미디에이션

--- _Script/AdmobADS.cs
+++ _Script/AdmobADS.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using GoogleMobileAds.Api;
 using UnityEngine.UI;
 using System;
+using GoogleMobileAds.Api.Mediation.IronSource;
 
 public class AdmobADS : MonoBehaviour
 {
@@ -28,7 +29,10 @@ public class AdmobADS : MonoBehaviour
 
     public GameObject GM;
 
-
+    private void Awake()
+    {
+        GoogleMobileAds.Mediation.IronSource.Api.IronSource.SetMetaData("do_not_sell", "true");
+    }
 
     // Use this for initialization 앱 ID
     void Start()
